0ac31d12b0da082e7c59392b576a26c716f70eb3
Douglas <mercurymuur86@yahoo.com>
2023-01-30 00:04:13 -0500

    love

---
+++ app.js
@@ -0,0 +1,51 @@
+    //variable 1
+const blackMamba = 24
+console.log(blackMamba)
+   
+    //variable 2
+const hisAirness = 23
+console.log(hisAirness)
+
+    // addition
+function theTwoGreatest (blackMamba, hisAirness) {
+return blackMamba+hisAirness
+}
+console.log(theTwoGreatest(24, 23))
+
+theTwoGreatest = (blackMamba, hisAirness)=> {
+return blackMamba+hisAirness
+}
+console.log(theTwoGreatest(24, 23))
+
+    //subtraction
+function champions (blackMamba, hisAirness) {
+return blackMamba - hisAirness
+}
+console.log(champions(blackMamba, hisAirness))
+
+champions  = (blackMamba, hisAirness)=> {
+return blackMamba - hisAirness
+}
+console.log(champions(blackMamba, hisAirness))
+
+    // multiplication
+function twoGuards (blackMamba, hisAirness) {
+    return (blackMamba * hisAirness)
+}  
+console.log(twoGuards(24, 23))
+
+twoGuards = (blackMamba, hisAirness) => {
+    return (blackMamba * hisAirness)
+}  
+console.log(twoGuards(24, 23))
+
+    //division
+function elevenCombinedChampionships(blackMamba, hisAirness){
+    return(blackMamba / hisAirness)
+}
+console.log(elevenCombinedChampionships(blackMamba, hisAirness))
+
+elevenCombinedChampionships = (blackMamba, hisAirness) => {
+    return(blackMamba / hisAirness)
+}
+console.log(elevenCombinedChampionships(blackMamba, hisAirness))
